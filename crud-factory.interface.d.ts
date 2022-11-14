0b21ec67b33d4d4b07701f49a266700923799636
hooman.the110 <hooman.the110@gmail.com>
2022-11-14 10:31:46 +0330

    folder structure added

--- crud-factory.interface.d.ts
+++ crud-factory.interface.d.ts
@@ -1,5 +1,5 @@
-import Creator from "./create.interface.js";
-import Deleter from "./delete.interface.js";
+import Creator from "./create/create.interface.js";
+import Deleter from "./delete/delete.interface.js";
 import Reader from "./read.interface.js";
 import Updater from "./update.interface.js";
 
